09214eb9d1b99eabf69090d51c2dd5e05a179074
Niedzwiedz1 <niewadzilukasz@gmail.com>
2023-12-06 13:44:18 +0000

    Create pdfs with first cell

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import pandas as pd
 import glob
 import fpdf
+from pathlib import Path
 
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    print(df)
-
+    pdf = fpdf.FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    filename = Path(filepath).stem
+    invoice_nr = filename.split("-")[0]
+    pdf.set_font(family="Times", style="B", size=12)
+    pdf.cell(w=50, h=8, txt=f"Invoice nr: {invoice_nr}", ln=1, align="L")
+    pdf.output(f"pdf/{filename}.pdf")
 
 
