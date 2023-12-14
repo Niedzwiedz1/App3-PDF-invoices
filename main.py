0e6a8d15ae32a1c5e3e52278b3a4fd5c82b07297
Niedzwiedz1 <niewadzilukasz@gmail.com>
2023-12-14 19:49:18 +0000

    Add comments

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 filepaths = glob.glob("invoices/*.xlsx")
 
+# Crete pdf files from excel file names
 for filepath in filepaths:
     pdf = fpdf.FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
@@ -12,14 +13,17 @@ for filepath in filepaths:
     filename = Path(filepath).stem
     invoice_nr, date = filename.split("-")
 
+# Add invoice number
     pdf.set_font(family="Times", style="B", size=12)
     pdf.cell(w=50, h=8, txt=f"Invoice nr: {invoice_nr}", ln=1, align="L")
 
+# Add invoice date
     pdf.set_font(family="Times", style="B", size=12)
     pdf.cell(w=50, h=8, txt=f"Date {date}", ln=1, align="L")
 
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
 
+# Add name of the table columns
     column_names = list(df.columns)
     column_names = [item.replace("_", " ").title() for item in column_names]
 
@@ -30,6 +34,7 @@ for filepath in filepaths:
     pdf.cell(w=30, h=8, txt=column_names[3], border=1)
     pdf.cell(w=30, h=8, txt=column_names[4], border=1, ln=1)
 
+# Add row to the table
     for index, row in df.iterrows():
 
         pdf.set_font(family="Times", style="B", size=10)
@@ -40,6 +45,7 @@ for filepath in filepaths:
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
+# Add sum of the prices
     total_sum = df["total_price"].sum()
     pdf.set_font(family="Times", style="B", size=10)
     pdf.cell(w=30, h=8, border=1)
@@ -48,9 +54,11 @@ for filepath in filepaths:
     pdf.cell(w=30, h=8, border=1)
     pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
 
+# Add sum sentence
     pdf.set_font(family="Times", style="B", size=12)
     pdf.cell(w=0, h=8, align="L", ln=1, txt=f"The total due amount is {total_sum} Euros")
 
+# Add company name and logo
     pdf.set_font(family="Times", style="B", size=12)
     pdf.cell(w=25, h=8, align="L", txt=f"Python How")
     pdf.image("logo.png", w=8)
