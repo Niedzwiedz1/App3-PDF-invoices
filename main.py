08bdd32bee1076c8628aab27865e396150c8523f
Niedzwiedz1 <niewadzilukasz@gmail.com>
2023-12-14 19:44:49 +0000

    Add header, total price, company and logo

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ for filepath in filepaths:
     pdf.cell(w=30, h=8, txt=column_names[4], border=1, ln=1)
 
     for index, row in df.iterrows():
+
         pdf.set_font(family="Times", style="B", size=10)
         pdf.set_text_color(80, 80, 80)
         pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
@@ -38,6 +39,21 @@ for filepath in filepaths:
         pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
+    total_sum = df["total_price"].sum()
+    pdf.set_font(family="Times", style="B", size=10)
+    pdf.cell(w=30, h=8, border=1)
+    pdf.cell(w=50, h=8, border=1)
+    pdf.cell(w=30, h=8, border=1)
+    pdf.cell(w=30, h=8, border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+    pdf.set_font(family="Times", style="B", size=12)
+    pdf.cell(w=0, h=8, align="L", ln=1, txt=f"The total due amount is {total_sum} Euros")
+
+    pdf.set_font(family="Times", style="B", size=12)
+    pdf.cell(w=25, h=8, align="L", txt=f"Python How")
+    pdf.image("logo.png", w=8)
 
     pdf.output(f"pdf/{filename}.pdf")
 
